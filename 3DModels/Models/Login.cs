34a99a1b8db8da723b2438c1a52d9b4e729ee164
VISHWAJA-SVG <121927329+VISHWAJA-SVG@users.noreply.github.com>
2023-06-23 17:12:58 +0530

    Update 2Login.cs

--- 3DModels/Models/Login.cs
+++ 3DModels/Models/Login.cs
@@ -8,14 +8,9 @@ namespace _3DModels.Models
     {
         public string Role { get; set; }
         
-       [Required(ErrorMessage = "Email ID is required.")]
-        [EmailAddress(ErrorMessage = "Invalid email address.")]
         public string EmailID { get; set; }
 
-        [Required(ErrorMessage = "Password is required.")]
-        [StringLength(16, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 16 characters.")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$", ErrorMessage = "Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character.")]
-        public string Password { get; set; }
+           public string Password { get; set; }
 
     }
 }
